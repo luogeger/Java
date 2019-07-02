117de1ece166718cc72356b3bf7c08ad89c51ea5
luoxiaoqing <luoxiaoqing@tuhu.cn>
2019-07-02 16:50:01 +0800

    update

--- JavaBase/src/main/java/com/company/h_list/Run.java
+++ JavaBase/src/main/java/com/company/h_list/Run.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author luoxiaoqing
@@ -28,5 +29,23 @@ public class Run {
         c.addAll(b);
         System.out.println(c.toArray());
 
+    }
+
+
+    @Test
+    public void test2 () {
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+
+        Collections.addAll(a, 1, 3, 5, 7, 9, 9);
+        Collections.addAll(b, 2, 4, 6, 7, 7, 9);
+        System.out.println(a);
+        System.out.println(b);
+        a.retainAll(b);
+        b.retainAll(a);
+        a = a.stream().distinct().collect(Collectors.toList());
+        b = b.stream().distinct().collect(Collectors.toList());
+        System.out.println(a);
+        System.out.println(b);
     }
 }
