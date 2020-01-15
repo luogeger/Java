5eb8357e9c49a7f12552fc10adfe6378eb383ff5
luogeger <xiaoqing1232@163.com>
2020-01-15 17:07:59 +0800

    'update'

---
+++ JavaBase/src/main/java/com/company/abstractKeyWord/a/MyInterface.java
@@ -0,0 +1,12 @@
+package com.company.abstractKeyWord.a;
+
+/**
+ * @author luoxiaoqing
+ * @date 2020-01-15__14:46
+ */
+public interface MyInterface {
+
+    void eat();
+
+    void fly();
+}
---
+++ JavaBase/src/main/java/com/company/abstractKeyWord/a/MySubClass.java
@@ -0,0 +1,15 @@
+package com.company.abstractKeyWord.a;
+
+/**
+ * @author luoxiaoqing
+ * @date 2020-01-15__14:47
+ */
+public class MySubClass extends MyAbstractClass {
+
+    @Override
+    public void run() {
+
+    }
+
+    //  子类继承抽象类
+}
