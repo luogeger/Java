8eacf66d177e1419c8e972809236ec5972efd21b
luogeger <xiaoqing1232@163.com>
2020-03-04 11:53:18 +0800

    update

---
+++ seckill-demo/src/main/java/com/first/seckill/seckilldemo/entity/Seckill.java
@@ -0,0 +1,7 @@
+package com.first.seckill.seckilldemo.entity;
+
+/**
+ * @author luoxiaoqing
+ */
+public class Seckill {
+}
---
+++ seckill-demo/src/main/java/com/first/seckill/seckilldemo/service/SeckillService.java
@@ -0,0 +1,23 @@
+package com.first.seckill.seckilldemo.service;
+
+import com.first.seckill.seckilldemo.entity.Seckill;
+
+/**
+ * @author luoxiaoqing
+ */
+public interface SeckillService {
+
+    int deleteById(Integer id);
+
+    int insert(Seckill seckill);
+
+    int insertSelective(Seckill seckill);
+
+    Seckill selectById(Integer id);
+
+    int updateBySelective(Seckill seckill);
+
+
+
+
+}
