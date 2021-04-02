343c7c4c5711172bfa0343c3d1cc38e741edb705
luogeger <xiaoqing1232@163.com>
2021-04-02 18:42:37 +0800

    update

---
+++ Concurrence/src/main/java/com/first/interview/M3Condition.java
@@ -0,0 +1,4 @@
+package com.first.interview;
+
+public class M3Condition {
+}
---
+++ Concurrence/src/main/java/com/first/juc/M7LockSupport.java
@@ -0,0 +1,46 @@
+package com.first.juc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public class M7LockSupport {
+
+    private volatile List<Integer> list = new ArrayList<>();
+
+    private void add(Integer i) {
+        list.add(i);
+    }
+
+    private int size() {
+        return list.size();
+    }
+
+    public static void main(String[] args) {
+        M7LockSupport m = new M7LockSupport();
+
+        Thread add = new Thread(() -> {
+            System.out.println("add start");
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+                m.add(i);
+                if (m.size() == 5) {
+
+                }
+            }
+            System.out.println("add end");
+        }, "add");
+
+
+        Thread monitor = new Thread(() -> {
+            System.out.println("monitor start");
+
+            System.out.println("monitor end");
+        }, "monitor");
+
+
+
+    }
+}
